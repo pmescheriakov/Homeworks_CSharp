07cf0c06684f18804e3f26d420b718af9dd8a5c7
Pavel_M <m3scheriakov@yandex.ru>
2023-11-28 16:42:35 +0300

    Добавил обработку ввода

--- Homework_1.cs
+++ Homework_1.cs
@@ -13,10 +13,20 @@ class Homework_1
     static void Main()
     {
         Console.Write("Введите первое число: ");
-        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number1;
+        while (!double.TryParse(Console.ReadLine(), out number1))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите первое число: ");
+        }
 
         Console.Write("Введите второе число: ");
-        double number2 = Convert.ToDouble(Console.ReadLine());
+        double number2;
+        while (!double.TryParse(Console.ReadLine(), out number2))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите второе число: ");
+        }
 
         Console.WriteLine($"Сумма: {Calculator.Add(number1, number2)}");
         Console.WriteLine($"Разность: {Calculator.Subtract(number1, number2)}");
